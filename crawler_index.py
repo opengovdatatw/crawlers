65880d0dc677ffd63901dca367e2816523ddbe07
york56 <36190560+york56@users.noreply.github.com>
2019-01-24 11:37:57 +0800

    Update crawler_index.py

--- crawler_index.py
+++ crawler_index.py
@@ -6,6 +6,7 @@ To get id
 import requests
 from bs4 import BeautifulSoup
 import pickle
+import datetime
 
 #url1 = 'https://movies.yahoo.com.tw/chart.html'
 #resp1 = requests.get(url1)
@@ -19,13 +20,11 @@ import pickle
 
 
 url_list = []
-for p_number in range(1,82):
-    url_list.append(['https://data.gov.tw/suggests?page='+str(p_number)])
-#print(url_list)
+for p_number in range(0,82): # page 0 to 81, 最終頁數目前是手動加上去
+    url_list.append('https://data.gov.tw/suggests?page='+str(p_number))
 
 id_list = []
 for url in url_list:    
-    url = 'https://data.gov.tw/suggests?page=81'
     resp = requests.get(url)
     resp.encoding = 'utf-8' # encoded with format utf-8 for chinese character
     soup = BeautifulSoup(resp.text, 'lxml')
@@ -33,10 +32,12 @@ for url in url_list:
     #tbody_section.get('href')1
     for tag in atags:
         if (tag.get('href')[0:6] == '/node/' 
-        and tag.get('href')[6:] != "37299"):
+        and tag.get('href')[6:] != "37299"): # 排除"政府網站資料開放宣告"
             print(tag.get('href')[6:])
             id_list.append(tag.get('href')[6:])
 
-if 0:
-    with open("id_list.pylist", "wb") as fp:   #Pickling
-        pickle.dump(id_list, fp)
+t = datetime.datetime.now()
+time_str = repr(t)[18:41]
+
+with open('id_list_' + time_str + '.pylist', "wb") as fp: #Pickling
+    pickle.dump(id_list, fp)
