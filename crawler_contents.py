5c451297f392e7de87fa1d5f25be0288e128bd56
york56 <36190560+york56@users.noreply.github.com>
2019-01-24 11:32:25 +0800

    time-stamped filename
    To save the file by time-stamped filename.
    Changing some names of variables.

--- crawler_contents.py
+++ crawler_contents.py
@@ -7,17 +7,22 @@ Given id, to get contents
 import requests
 from bs4 import BeautifulSoup
 import pickle
+import datetime
+import csv
+
 
 with open("id_list.pylist", "rb") as fp:   # Unpickling
     id_list = pickle.load(fp)
 
 label_list = ['ID', '建議之標題', '建議資料集名稱', '建議開放的欄位', '建議原因', '派發機關', '回復狀態', '分派狀態', '回復時間', '回應']
-#    reply_1_title, reply_1_acc, reply_1_time, reply_1_conten
-#    reply_2_title, reply_2_acc, reply_2_time, reply_2_conten
-#    reply_3_title, reply_3_acc, reply_3_time, reply_3_conten
+
 all_contents = []
 all_contents.append(label_list)
-for id in id_list:
+
+
+for id in id_list: 
+    print(id)
+    
     single_list = []
     
     single_list.append(id)    
@@ -33,27 +38,21 @@ for id in id_list:
 
     # 欄位內容 
     field_item = soup.find_all('div', class_='field-item')
-    for row in field_item[0:7]:
-        single_list.append(row.string)
-        
+    for field_item_row in field_item[0:7]:
+        single_list.append(field_item_row.string)
+    
+    # 回應之內容
     comments_content = soup.find_all('div', class_='field field-name-comment-body field-type-text-long field-label-hidden')
-    for row in comments_content:
-        rowrow = row.find('p')
-        single_list.append(rowrow)
+    for sub_comments_content in comments_content:
+        sub_sub_comments_content = sub_comments_content.find('p')
+        single_list.append(sub_sub_comments_content)
     
     all_contents.append(single_list)
 
+t = datetime.datetime.now()
+time_str = repr(t)[18:41]
 
-
-import csv
-with open('all_contents.csv', 'w') as csvFile:
+with open('all_contents_' + time_str + '.csv', 'w') as csvFile:
     writer = csv.writer(csvFile)
     writer.writerows(all_contents)
 csvFile.close()
-
-
-
-
-
-
-
