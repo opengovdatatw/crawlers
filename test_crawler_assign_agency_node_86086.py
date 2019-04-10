1499db1d213b665d81f40463d35a1cc16c2bbfed
york56 <york13468@gmail.com>
2019-04-10 13:55:15 +0800

    Complete the test

--- test_crawler_assign_agency_node_86086.py
+++ test_crawler_assign_agency_node_86086.py
@@ -8,9 +8,7 @@ Get { ID | numbers of agency assigned | names of those agencies }
 """
 import requests
 from bs4 import BeautifulSoup
-import datetime
 import csv
-
 
 id_list = [['86086']] 
 
@@ -25,27 +23,29 @@ for id in id_list:
 
     single_list = []  
 
-    for things in single_list: 
-        
-        # Put the ID in this single_list
-        single_list.append(id_str)  # generate URL
+    # Put the ID in this single_list
+    single_list.append(id_str)  # generate URL
+
+    # Get the SOUP
+    url = 'https://data.gov.tw/node/' + id_str
+    resp = requests.get(url)
+    resp.encoding = 'utf-8' # encoded with format utf-8 for chinese character
+    soup = BeautifulSoup(resp.text, 'lxml')
+
+    # Get the 派發機關
+    assign_agency = soup.find_all(
+        'div', class_='field field-name-field-assign-agency field-type-entityreference field-label-inline clearfix'
+        )[0].find_all(
+                'div', class_='field-items'
+                )[0].find_all(
+                        'div')
     
-        # Get the SOUP
-        url = 'https://data.gov.tw/node/' + id_str
-        resp = requests.get(url)
-        resp.encoding = 'utf-8' # encoded with format utf-8 for chinese character
-        soup = BeautifulSoup(resp.text, 'lxml')
+    # Put the number of agencies in this single_list
+    single_list.append(len(assign_agency))
     
-        # Get the 派發機關
-        assign_agency = soup.find_all(
-            'div', class_='field field-name-field-assign-agency field-type-entityreference field-label-inline clearfix'
-            )[0].find_all(
-                    'div', class_='field-items'
-                    )[0].find_all(
-                            'div')
-        
-        # Put the number of agencies in this single_list
-        number_assign_agency = len(assign_agency)
+    # Put the names of agencies in this single_list
+    for every_item in assign_agency:
+        single_list.append(every_item.string)
     
     # Add this single_list to the chart
     all_contents.append(single_list)
